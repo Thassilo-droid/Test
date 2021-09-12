a1f126b65b03f700775abb180f616572d3a97faa
Thassilo-droid <thassilo.horn@gmx.de>
2021-09-12 16:30:03 +0200

    elif

---
+++ t.py
@@ -0,0 +1,8 @@
+x = int(input("Geben sie eine Zahl ein: "))
+
+if x < 50:
+    print(str(x) + " ist kleiner als 50")
+elif x < 100:
+    print(str(x) + " ist kleiner als 100")
+else:
+    print(str(x) + " ist größer als 100")
